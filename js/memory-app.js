ddaf1350e17d9b82be156e111872f298678fcee8
Lewis Lockhart <lewis00001@gmail.com>
2020-06-26 00:31:21 -0600

    disabled clicks for user while puzzed is in view

--- js/memory-app.js
+++ js/memory-app.js
@@ -82,6 +82,7 @@ $(document).ready(function () {
     let loadedPuzzleValues = [];
     let userPuzzleValues = [];
     let memActiveColor = "white";
+    let currentPuzzleId = "";
 
     // compares arrays and returns a bool
     function match(arr1, arr2) {
@@ -99,12 +100,16 @@ $(document).ready(function () {
     // runs countdown timer for peek button
     peekCountDown = (time) => {
         let t = time;
+        // disables users ability to click puzzle dots
+        $('.m-dot').addClass('non-click');
         reduce = () => {
             $('#peek').html(t);
             if (t <= 0) {
                 clearInterval(interval);
                 $('#peek').html('PEEK -100pts');
                 $('#load-puzzle').addClass('display-off');
+                // enables users ability to click puzzle dots
+                $('.m-dot').removeClass('non-click');
             }
             t--;
         };
@@ -159,6 +164,7 @@ $(document).ready(function () {
             points += points_base;
             updateTotalScore();
             $('#load-puzzle').removeClass('display-off');
+            $(currentPuzzleId).removeClass('memory-sel-button-current').addClass('memory-sel-button-complete');
         } else {
             $('.info-output').text('Oops, try again').addClass('error');
             setTimeout(function () {
@@ -172,13 +178,17 @@ $(document).ready(function () {
         loadedPuzzleValues = [];
         for (let i = 0; i < 16; i++) {
             let memCurrentDot = '#user-dot-' + i;
-            $(memCurrentDot).removeClass().addClass("dot circ off");
+            $(memCurrentDot).removeClass().addClass("dot m-dot circ off");
+            $(memCurrentDot).attr('value', 0);
         };
     }
 
     // loads the selected puzzle
     $('.memory-sel-button').on('click', function (e) {
         clearUserDots();
+        currentPuzzleId = "#" + $(this).attr('id');
+        console.log(currentPuzzleId);
+        $(this).addClass('memory-sel-button-current non-click')
         let level = $(this).attr('value');
         loadedPuzzleValues = [];
         for (let i = 0; i < 16; i++) {
